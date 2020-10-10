d19e6894cd8360c87f783784e42a0bb4a79036a7
Brandon Moon <bmoon@omadi.com>
2020-10-10 13:36:27 -0600

    Customize home page and delete example posts

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,17 +1,11 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Artisan Static</h1>
+    <h1>Welcome to Brandon Moon's Website</h1>
 
-    <p>Artisan Static is a starter template for building a static Jigsaw blog hosted on Netlify.</p>
+    <a href="https://lh3.googleusercontent.com/1ONG5kdextiSCeH7uMgusOC_c3MhnHgK5fTVTkkG96Ntyp66yU7d2MX0g16L7lf4GjaRu02naRNr2D3f0JCLyx_uu0i8tmJDKs_DAaM_e3ZIv_B3ODx-eWoSLc6Gb9hlCtHGW2haGNc=w2400"><img src="https://lh3.googleusercontent.com/1ONG5kdextiSCeH7uMgusOC_c3MhnHgK5fTVTkkG96Ntyp66yU7d2MX0g16L7lf4GjaRu02naRNr2D3f0JCLyx_uu0i8tmJDKs_DAaM_e3ZIv_B3ODx-eWoSLc6Gb9hlCtHGW2haGNc=w820-h415-p-k" /></a>
 
-    <p>This comes with code highlighting, share buttons, comments, analytics, an RSS feed, a contact form, a CMS and more.</p>
+    <p>Hi, I'm Brandon Moon, and I'm a mobile software developer. I currently work with React Native but have done native development for both iOS and Android and love to learn new technologies. I'm also a husband and father to three wonderful children. </p>
 
-    <p>The HTML, CSS and JavaScript in this template are extremely minimal, which makes the code easy to build on top of or replace completely.</p>
-
-    <h2 id="test">Asset Compilation Test</h2>
-
-    <p>CSS test: <span class="test-css">text with blue border</span></p>
-
-    <p>JavaScript test: <a href="#test" class="test-js">click me</a></p>
+    <p>This website is just a place for me to experiment and learn, as well as a convenient place for links when I need them.</p>
 @endsection
