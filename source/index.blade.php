40a4f95f6911ab75a4573092b12b97d6054645f8
Brandon Moon <bmoon@omadi.com>
2020-10-12 09:09:01 -0600

    Move home screen photo to top

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,9 +1,11 @@
 @extends('_layouts.master')
 
 @section('content')
-    <h1>Welcome to Brandon Moon's Website</h1>
+    <figure>
+        <img src="https://res.cloudinary.com/df4nwdhdu/image/upload/v1602359632/family.jpg" />
+    </figure>
 
-    <img src="https://res.cloudinary.com/df4nwdhdu/image/upload/v1602359632/family.jpg" />
+    <h1>Welcome to Brandon Moon's Website</h1>
 
     <p>Hi, I'm Brandon Moon, and I'm a mobile software developer. I currently work with React Native but have done native development for both iOS and Android and love to learn new technologies. I'm also a husband and father to three wonderful children. </p>
 
