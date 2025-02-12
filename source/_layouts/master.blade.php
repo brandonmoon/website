d790ab90986d936f9547d8e5c4c86471405d58a4
Brandon Moon <brandonmoon@users.noreply.github.com>
2025-02-11 19:22:06 -0700

    Update master.blade.php

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -9,7 +9,7 @@
         {{ !empty($__env->yieldContent('title')) ? ' | ' : '' }}
         {{ $page->site->title }}
     </title>
-    <script src="https://www.google.com/recaptcha/api.js?render=6LfPVeUfAAAAAOuLuXnr8-6DHWS6fg32guoEGhgh"></script>
+    <script src="https://www.google.com/recaptcha/enterprise.js?render=6LfPVeUfAAAAAOuLuXnr8-6DHWS6fg32guoEGhgh"></script>
 
     @include('_partials.head.favicon')
     @include('_partials.head.meta')
